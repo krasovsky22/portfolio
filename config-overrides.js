d20a3e0b38542b3de138c083fdaed2a4c55e0f80
vladk <vlad_krasovsky@yahoo.com>
2020-08-08 21:54:18 -0700

    initial commit

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { override, addWebpackAlias } = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+  addWebpackAlias({
+    ['@components']: path.resolve(__dirname, './src/components'),
+    ['@views']: path.resolve(__dirname, './src/views'),
+    ['@assets']: path.resolve(__dirname, './src/assets'),
+  })
+);
