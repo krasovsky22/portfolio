7ddf0a46e126fd2e9116b2ec57c025d2bdfee604
vladk <vlad_krasovsky@yahoo.com>
2020-08-12 23:10:38 -0700

    added firebase and loadable hook

--- src/views/Education.tsx
+++ src/views/Education.tsx
@@ -53,24 +53,10 @@ const Education: React.FC = () => {
           </Card>
         </Col>
 
-        <Col sm={12} md={6}>
-          <Card className="experience-card mb-3">
-            <CardTitle>
-              <CardHeader>
-                <h4>2018 - 2019</h4>
-                <h3>Software Engineering</h3>
-              </CardHeader>
-            </CardTitle>
-            <CardBody>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Molestias voluptates esse rem inventore similique cumque nostrum
-                explicabo. Pariatur vitae eveniet culpa officiis inventore ea
-                totam fugiat nulla similique, aperiam impedit!
-              </p>
-            </CardBody>
-          </Card>
-        </Col>
+const Education: React.FunctionComponent<injectedLoadableProps<
+  PreviouExperienceType
+>> = ({ data }: injectedLoadableProps<PreviouExperienceType>) => {
+  console.log('here', data);
 
         <Col sm={12} md={6}>
           <Card className="experience-card mb-3">
