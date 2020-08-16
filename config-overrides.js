c83454002b98be134b48c90e4f86bf7a879dc84e
vladk <vlad_krasovsky@yahoo.com>
2020-08-15 19:49:09 -0700

    Revert "added could function"
    This reverts commit b7f63527fcf0e6b0c1e1a4686d02c9b09fa721c6.

--- config-overrides.js
+++ config-overrides.js
@@ -9,6 +9,5 @@ module.exports = override(
     '@views': path.resolve(__dirname, './src/views'),
     '@assets': path.resolve(__dirname, './src/assets'),
     '@database': path.resolve(__dirname, './src/database'),
-    '@functions': path.resolve(__dirname, './src/functions'),
   })
 );
